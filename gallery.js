787cd8dd2c9378fb99ce61eaf256a8f8a0db9b5d
owen-caldwell <77856442+owen-caldwell@users.noreply.github.com>
2023-07-17 20:51:59 -0400

    latest baby!

--- gallery.js
+++ gallery.js
@@ -1,47 +1,20 @@
-// FOR NEXT/BACK BUTTONS
-let slideIndex = 1;
-function showDivs(n) {
-    let i;
-    const item = document.getElementsByClassName("slideshow-item");
-    if (n > item.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = item.length}
-    for (i = 0; i < item.length; i++) {
-        item[i].style.display = "none";  
+const content = document.getElementById('container');
+
+function isWindowWidthGreaterThan1000px() {
+  return window.innerWidth > 1000;
+}
+
+function handleScroll(event) {
+  if (isWindowWidthGreaterThan1000px()) {
+    if (event.deltaY < 0) {
+      content.scrollLeft += 700;
+    } else if (event.deltaY > 0) {
+      content.scrollLeft -= 700;
     }
-    item[slideIndex-1].style.display = "flex";  
+  }
 }
-showDivs(slideIndex);
 
-function plusDivs(n) {
-    showDivs(slideIndex += n);
+// Add the scroll event listener conditionally
+if (isWindowWidthGreaterThan1000px()) {
+  window.addEventListener('wheel', handleScroll);
 }
-// Old JS ^
-
-// FOR SWIPE (ChatGPT):
-
-const slider = document.querySelector('.slider');
-let isDown = false;
-let startX;
-let scrollLeft;
-
-slider.addEventListener('mousedown', (e) => {
-  isDown = true;
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-});
-
-slider.addEventListener('mouseleave', () => {
-  isDown = false;
-});
-
-slider.addEventListener('mouseup', () => {
-  isDown = false;
-});
-
-slider.addEventListener('mousemove', (e) => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 3; // Adjust scrolling speed here
-  slider.scrollLeft = scrollLeft - walk;
-});
--- randombackground.js
+++ randombackground.js
@@ -1,6 +1,6 @@
 // random background color
 function randomBg() { // function definition
-    const background = document.getElementById('bgimg');
+    let background = document.getElementById('bgimg');
     let randomBackground = Math.floor(Math.random() * 4) + 1;
     console.log('Random hue:', randomBackground);
     // apply color with style object using HSL color mode
