d9ad5989c5ab3e36cb8c59ea34b3ee7225b8fc49
Owen Caldwell <caldwellom@gmail.com>
2024-11-04 20:53:46 -0500

    added photos page

--- randombackground.js
+++ randombackground.js
@@ -1,7 +1,7 @@
 // random background color
 function randomBg() { // function definition
     let background = document.getElementById('bgimg');
-    let randomBackground = Math.floor(Math.random() * 4) + 1;
+    let randomBackground = Math.floor(Math.random() * 3) + 1;
     console.log('Random hue:', randomBackground);
     // apply color with style object using HSL color mode
     background.src = 'background/' + randomBackground + '.gif';
